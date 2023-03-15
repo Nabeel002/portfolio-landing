0a9dcf91a77c47d44764361364bd10a68e5a55da
nabeel siddiqui <nabeelsiddiquizw@gmail.com>
2023-03-16 05:14:22 +0530

    responsiveness done

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("ham").addEventListener("click",()=>{
+   
+   document.getElementById("mob").style.display="flex"
+
+
+})
+
+
+
+document.getElementById("cross").addEventListener("click",()=>{
+
+
+    document.getElementById("mob").style.display="none"
+})
