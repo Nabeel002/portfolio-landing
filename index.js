3e402df1c77c27edf1dabf7c0d6eb85efa2f4904
nabeel siddiqui <nabeelsiddiquizw@gmail.com>
2023-03-17 03:03:17 +0530

    responsive done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.getElementById("ham").addEventListener("click",()=>{
         document.getElementById("mob").classList.remove("transformation");
 
     document.getElementById("mob").classList.add("transform");
+    document.getElementById("overlay-nav-mobile").style.display="block"
 
 
 })
@@ -13,6 +14,7 @@ document.getElementById("cross").addEventListener("click",()=>{
 
     document.getElementById("mob").classList.remove("transform")
         document.getElementById("mob").classList.add("transformation");
+        document.getElementById("overlay-nav-mobile").style.display = "none";
 
 
 })
